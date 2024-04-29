8cd7977bb04d87068097bbc6a5abe19315e7bb07
oseiasdfarias <oseias.dfarias@gmail.com>
2024-04-28 21:48:28 -0300

    add new project in repo

--- projeto-03/diagrama.py
+++ projeto-03/diagrama.py
@@ -3,9 +3,7 @@ from diagrams import Diagram
 from diagrams.custom import Custom
 from diagrams.aws.cost import Budgets as BG
 from diagrams.aws.compute import Lambda as LB
-from diagrams.aws.engagement import SimpleEmailServiceSes as SES
 
 with Diagram("Web Service", show=False):
     csv = Custom("Telegram", "./utils/telegram.png")
     BG("Cost Explorer") >> LB("Função Lambda") >> csv
-
